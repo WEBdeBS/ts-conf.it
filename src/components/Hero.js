fc8d9818ffc6552222c3e4c4934ef0404e345417
Francesco Falanga <falanga.fra@gmail.com>
2019-06-01 11:59:44 +0200

    fix: typo

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -14,7 +14,7 @@ const Hero = class extends React.Component {
         <div className="container">
           <div className="row align-items-center">
             <div className="col-md-10">
-              <span className="d-block mb-3 caption" data-aos-disabled="fade-up" data-aos-delay="100">Indipendent Italian TypeScript Conference</span>
+              <span className="d-block mb-3 caption" data-aos-disabled="fade-up" data-aos-delay="100">Independent Italian TypeScript Conference</span>
               <h1 className="d-block mb-4" data-aos-disabled="fade-up" data-aos-delay="200">TSconf 2019</h1>
               <span className="d-block mb-5 caption" data-aos-disabled="fade-up" data-aos-delay="300">October 25th, Desenzano del Garda, Italy</span>
               {/*<a href="#" className="btn-custom" data-aos-disabled="fade-up" data-aos-delay="400"><span>Buy Tickets</span></a>*/}
