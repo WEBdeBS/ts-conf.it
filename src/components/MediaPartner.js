d039e1220b29b90e82858ecd06ccec3fea2acbfb
Davide Brognoli <davide.brognoli@gmail.com>
2019-06-18 22:21:07 +0200

    Media partner

--- src/components/MediaPartner.js
+++ src/components/MediaPartner.js
@@ -3,7 +3,7 @@ import React from 'react'
 const MediaPartner = class extends React.Component {
   render() {
     return (
-      <div className="site-section">
+      <div className="site-section media-partner">
         <div className="container">
           <div className="row mb-5">
             <div className="col-lg-4 ">
@@ -12,24 +12,15 @@ const MediaPartner = class extends React.Component {
               </div>
             </div>
           </div>
-          <div className="row mb-5">
+          <div className="row mb-5 media-partner__logos">
             <div className="col-md-6 col-lg-4 mb-5 mb-lg-0" data-aos-disabled="fade" data-aos-delay="200">
-              <img src="/images/logo_1.png" alt="Logo1" className="img-fluid" />
+              <img src="/images/grusp.svg" alt="GRUSP" className="img-fluid img-full" />
             </div>
             <div className="col-md-6 col-lg-4 mb-5 mb-lg-0" data-aos-disabled="fade" data-aos-delay="300">
-              <img src="/images/logo_2.png" alt="Logo2" className="img-fluid" />
+              <img src="/images/brewbox.svg" alt="Brewbox" className="img-fluid img-full" />
             </div>
             <div className="col-md-6 col-lg-4 mb-5 mb-lg-0" data-aos-disabled="fade" data-aos-delay="400">
-              <img src="/images/logo_3.png" alt="Logo3" className="img-fluid" />
-            </div>
-            <div className="col-md-6 col-lg-4 mb-5 mb-lg-0" data-aos-disabled="fade" data-aos-delay="500">
-              <img src="/images/logo_4.png" alt="Logo4" className="img-fluid" />
-            </div>
-            <div className="col-md-6 col-lg-4 mb-5 mb-lg-0" data-aos-disabled="fade" data-aos-delay="600">
-              <img src="/images/logo_5.png" alt="Logo5" className="img-fluid" />
-            </div>
-            <div className="col-md-6 col-lg-4 mb-5 mb-lg-0" data-aos-disabled="fade" data-aos-delay="700">
-              <img src="/images/logo_6.png" alt="Logo6" className="img-fluid" />
+              <img src="/images/doitbetter.svg" alt="Do It Better" className="img-fluid img-full" />
             </div>
           </div>
         </div>
