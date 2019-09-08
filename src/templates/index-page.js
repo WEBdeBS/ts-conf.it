a44453a974c9aeb8b478dacbca222964c413c9ed
Davide Brognoli <davide.brognoli@gmail.com>
2019-09-08 15:27:05 +0200

    Remove unused code

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { Link, graphql } from 'gatsby'
+import { graphql } from 'gatsby'
 
 import Layout from '../components/Layout'
 import Header from '../components/Header'
 import Hero from '../components/Hero'
-import CallForPaper from '../components/CallForPaper'
+// import CallForPaper from '../components/CallForPaper'
 import Tickets from '../components/Tickets'
 import Speakers from '../components/Speakers'
 // import Schedule from '../components/Schedule'
@@ -15,8 +15,8 @@ import MediaPartner from '../components/MediaPartner'
 import Map from '../components/Map'
 import CodeOfConduct from '../components/CodeOfConduct'
 
-import Features from '../components/Features'
-import BlogRoll from '../components/BlogRoll'
+// import Features from '../components/Features'
+// import BlogRoll from '../components/BlogRoll'
 import Newsletter from '../components/Newsletter';
 
 export const IndexPageTemplate = ({
@@ -37,106 +37,6 @@ export const IndexPageTemplate = ({
     <MediaPartner></MediaPartner>
     <CodeOfConduct></CodeOfConduct>
     <Map></Map>
-    
-    <div style={{display: "none"}}>
-      <div>
-        <div
-          className="full-width-image margin-top-0"
-          style={{
-            // backgroundImage: `url(${
-            //   !!image.childImageSharp ? image.childImageSharp.fluid.src : image
-            // })`,
-            backgroundPosition: `top left`,
-            backgroundAttachment: `fixed`,
-          }}
-        >
-          <div
-            style={{
-              display: 'flex',
-              height: '150px',
-              lineHeight: '1',
-              justifyContent: 'space-around',
-              alignItems: 'left',
-              flexDirection: 'column',
-            }}
-          >
-            <h1
-              className="has-text-weight-bold is-size-3-mobile is-size-2-tablet is-size-1-widescreen"
-              style={{
-                boxShadow:
-                  'rgb(255, 68, 0) 0.5rem 0px 0px, rgb(255, 68, 0) -0.5rem 0px 0px',
-                backgroundColor: 'rgb(255, 68, 0)',
-                color: 'white',
-                lineHeight: '1',
-                padding: '0.25em',
-              }}
-            >
-              {title}
-            </h1>
-            <h3
-              className="has-text-weight-bold is-size-5-mobile is-size-5-tablet is-size-4-widescreen"
-              style={{
-                boxShadow:
-                  'rgb(255, 68, 0) 0.5rem 0px 0px, rgb(255, 68, 0) -0.5rem 0px 0px',
-                backgroundColor: 'rgb(255, 68, 0)',
-                color: 'white',
-                lineHeight: '1',
-                padding: '0.25em',
-              }}
-            >
-              {subheading}
-            </h3>
-          </div>
-        </div>
-        <section className="section section--gradient">
-          <div className="container">
-            <div className="section">
-              <div className="columns">
-                <div className="column is-10 is-offset-1">
-                  <div className="content">
-                    <div className="content">
-                      <div className="tile">
-                        <h1 className="title">{mainpitch.title}</h1>
-                      </div>
-                      <div className="tile">
-                        <h3 className="subtitle">{mainpitch.description}</h3>
-                      </div>
-                    </div>
-                    <div className="columns">
-                      <div className="column is-12">
-                        <h3 className="has-text-weight-semibold is-size-2">
-                          {heading}
-                        </h3>
-                        <p>{description}</p>
-                      </div>
-                    </div>
-                    <Features gridItems={intro.blurbs} />
-                    <div className="columns">
-                      <div className="column is-12 has-text-centered">
-                        <Link className="btn" to="/products">
-                          See all products
-                        </Link>
-                      </div>
-                    </div>
-                    <div className="column is-12">
-                      <h3 className="has-text-weight-semibold is-size-2">
-                        Latest stories
-                      </h3>
-                      <BlogRoll />
-                      <div className="column is-12 has-text-centered">
-                        <Link className="btn" to="/blog">
-                          Read more
-                        </Link>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </section>
-      </div>
-    </div>
   </div>
 )
 
